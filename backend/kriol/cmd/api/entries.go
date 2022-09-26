ab358abbcec4af2a99db3aab82a3f6059c58695e
michael <2018118090@ub.edu.bz>
2022-09-26 11:05:26 -0600

    added and tested school version of json api representation

--- backend/kriol/cmd/api/entries.go
+++ backend/kriol/cmd/api/entries.go
@@ -5,6 +5,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"kriol.michaelgomez.net/internal/data"
 )
 
 func (app *application) createEntryHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +21,22 @@ func (app *application) showEntryHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
-	//Displaying the id, for now
-	fmt.Fprintf(w, "show the details for school %d\n", id)
+
+	// Create a new instance of School struct containing the ID we extracted from out URL and some sample data
+	school := data.School{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Name:      "Apple Tree",
+		Level:     "High School",
+		Contact:   "Anna Smith",
+		Phone:     "601-4111",
+		Address:   "14 Apple street",
+		Mode:      []string{"blended", "online"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, school, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encounterd a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
