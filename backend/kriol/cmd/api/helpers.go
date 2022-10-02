66fd108cc58cb68e8ab67790d8234bdbf82df5e5
michael <2018118090@ub.edu.bz>
2022-10-02 13:12:10 -0600

    better request handling

--- backend/kriol/cmd/api/helpers.go
+++ backend/kriol/cmd/api/helpers.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -46,8 +47,14 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 }
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	//Use http.MaxBytesReader() to limit the size of the request body to 1 MB
+	maxBytes := 1_048_576
+
 	//Decode the request body into the target destination
-	err := json.NewDecoder(r.Body).Decode(dst)
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	err := dec.Decode(dst)
 
 	//Check for a bad request
 	if err != nil {
@@ -72,6 +79,15 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
 
+		//Unmappable field
+		case strings.HasPrefix(err.Error(), "json: unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contains unkown key %s", fieldName)
+
+		//Too large
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
+
 		//Pass non-nil pointer error
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
@@ -80,5 +96,12 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 			return err
 		}
 	}
+
+	//Call Decode again
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single value")
+	}
+
 	return nil
 }
