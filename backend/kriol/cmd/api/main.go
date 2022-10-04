04261104f12353caf9da7c71fff8d9c833c91d84
michael <2018118090@ub.edu.bz>
2022-10-04 14:11:10 -0600

    added means of database connection

--- backend/kriol/cmd/api/main.go
+++ backend/kriol/cmd/api/main.go
@@ -3,12 +3,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 // version number
@@ -17,7 +21,10 @@ const version = "1.0.0"
 // configuration settings
 type config struct {
 	port int
-	env  string //development, staging, production
+	env  string   //development, staging, production
+	db   struct { //database field
+		dsn string
+	}
 }
 
 // dependency injection
@@ -32,11 +39,19 @@ func main() {
 	//reading the flags
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development | staging | production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("APPLETREE_DB_DSN"), "PostgreSQL DSN")
 	flag.Parse()
 
 	//creating logger
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
+	//create the connection pool
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
 	//instance of app struct
 	app := &application{
 		config: cfg,
@@ -54,6 +69,23 @@ func main() {
 
 	//starting our server
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+// OpenDB() function returns a *sql.DB connection pool
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	//Create a context with a 5-second timeout deadline
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
