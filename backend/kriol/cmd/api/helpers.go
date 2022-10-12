c438ca1ae34b22145605c6644f4734d37180750b
michael <2018118090@ub.edu.bz>
2022-10-11 19:32:58 -0600

    Added all the helper functions for parsing query parameters

--- backend/kriol/cmd/api/helpers.go
+++ backend/kriol/cmd/api/helpers.go
@@ -8,10 +8,12 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
 	"github.com/julienschmidt/httprouter"
+	"kriol.michaelgomez.net/internal/validator"
 )
 
 // Define a new type named envelope
@@ -104,4 +106,45 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 	}
 
 	return nil
+}
+
+// The readString() method returns a string value from the query parameters
+// String or returns a default value if no matching key is found
+func (app *application) readString(qs url.Values, key string, defaultValue string) string {
+	//Get the value
+	value := qs.Get(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
+
+// The readCSV() method splits a value into a slice based on the comma separator.
+// if no matching key is found then the default value is returned
+func (app *application) readCSV(qs url.Values, key string, defaultValue []string) []string {
+	//Get the value
+	value := qs.Get(key)
+	if value == "" {
+		return defaultValue
+	}
+
+	//Split the string based on the "," delimiter
+	return strings.Split(value, ",")
+}
+
+// The readInt() method converts a string value from the query string to an integer value
+// if the value cannot be converted to an integer then a validation error is added to the validation errors map
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+	//Get the value
+	value := qs.Get(key)
+	if value == "" {
+		return defaultValue
+	}
+	//Perform the conversion to an integer
+	intValue, err := strconv.Atoi(value)
+	if err != nil {
+		v.AddError(key, "must be an interger value")
+		return defaultValue
+	}
+	return intValue
 }
