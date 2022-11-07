62d0756fa5f412033471bcfa9138cc0f8a22280b
michael <2018118090@ub.edu.bz>
2022-11-07 15:46:52 -0600

    changed rate limiter to both client and request based

--- backend/kriol/cmd/api/middleware.go
+++ backend/kriol/cmd/api/middleware.go
@@ -4,7 +4,10 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
@@ -22,12 +25,56 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	limit := rate.NewLimiter(2, 4) // 1 request / 0.5 seconds
+	//Create a client type
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	//Launch a background goroutine that removes old entries
+	//from the clients map once every minute
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			//Lock before starting to clean
+			mu.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limit.Allow() {
+		//Get the ip address of the request
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+		//lock()
+		mu.Lock()
+		//check if the IP address is in the map
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+		//Update the last seen time of the client
+		clients[ip].lastSeen = time.Now()
+
+		//check if request allowed
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+		mu.Unlock()
 		next.ServeHTTP(w, r)
 	})
 }
