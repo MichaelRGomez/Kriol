c50f5e1e0ba41f93160320eb38df3f1adcaafab2
michael <2018118090@ub.edu.bz>
2022-11-09 22:19:44 -0600

    started working on users

---
+++ backend/kriol/internal/data/users.go
@@ -0,0 +1,53 @@
+//Filename: internal/data/users.go
+
+package data
+
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	version   int       `json:"-"`
+}
+
+// Create a customer password type
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+// The set() method stores the has of the plaintext password
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+
+	return nil
+}
+
+// The matches() method checks if the supplied password is correct
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+	return true, nil
+}
